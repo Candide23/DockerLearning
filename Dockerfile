ec18cd97eb6cc4f52b18e4f7c574b4a0d40b14fe
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-09-29 11:00:24 -0500

    learned about containers, dockerfile and images

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM node:alpine
 
 WORKDIR /usr/src/app
 
+COPY ./package.json .
+
+RUN npm install
+
 COPY . .
 
 RUN npm install
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const post = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Goodbye World!!!")
+    res.send("Goodbye World !!!!")
 })
 
 
